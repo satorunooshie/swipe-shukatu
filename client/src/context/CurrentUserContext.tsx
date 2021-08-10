33b2413e55efc0c08cf50d71da5c356e79560d00
hiro <hiromhiro617@gmail.com>
2021-08-11 00:29:47 +0900

    fix

--- client/src/context/CurrentUserContext.tsx
+++ client/src/context/CurrentUserContext.tsx
@@ -1,6 +1,6 @@
 import firebase from "firebase";
 import { FC, createContext, useEffect, useState } from "react";
-import { useShowToast } from "../hooks/useShowToast"
+import { useShowToast } from "../hooks/useShowToast";
 
 // 仮
 type User = {
@@ -20,13 +20,13 @@ const CurrentUserProvider: FC = ({ children }) => {
   const [currentUser, setCurrentUser] = useState<User | null | undefined>(
     undefined
   );
-  const showToast = useShowToast()
+  const showToast = useShowToast();
 
   useEffect(() => {
     // マウント時にログインをチェック
     firebase.auth().onAuthStateChanged(async (user) => {
       if (user) {
-        showToast(`uid: ${user.uid} ログインしました`)
+        showToast(`uid: ${user.uid} ログインしました`);
 
         // TODO： API叩く処理
         setCurrentUser({ uid: user.uid });
