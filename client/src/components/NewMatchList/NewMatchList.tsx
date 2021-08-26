2d1d0a0e810f6893ba2d1d416b6d8bb4e6243b03
hiromu <hiromhiro617@gmail.com>
2021-08-27 01:15:11 +0900

    filter ltd

--- client/src/components/NewMatchList/NewMatchList.tsx
+++ client/src/components/NewMatchList/NewMatchList.tsx
@@ -1,43 +1,93 @@
-import { VFC } from "react";
-import { Flex, Stack, Text, Image, VStack } from "@chakra-ui/react";
+import { VFC, useState, useEffect } from "react";
+import {
+  Flex,
+  Stack,
+  Text,
+  Image,
+  VStack,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Button,
+} from "@chakra-ui/react";
 import { MAIN_COLOR } from "../../constants/MainColor";
 import { NavLink } from "react-router-dom";
 import { Ltd } from "../../type/Ltd";
 import LoadingNewMatchList from "./LoadingNewMatchList";
+import MatchListWrap from "../MatchListWrap/MatchListWrap";
 import { useNewMatch } from "./useNewMatch";
+import { ChevronDownIcon, CheckIcon } from "@chakra-ui/icons";
 
 const NewMatchList: VFC = () => {
   const { ltds, error } = useNewMatch();
+  const [ltdList, setLtdList] = useState(ltds);
+  const [ltdType, setLtdType] = useState<"all" | "like" | "slike">("all");
+
+  useEffect(() => {
+    if (ltdType === "like") {
+      // Todo:
+      // setLtdList(ltds.filter((ltd: Match) => ltd.reaction_type === 1));
+      setLtdList(ltds.filter((ltd: Ltd) => ltd.joke.length % 2 === 0));
+    } else if (ltdType === "slike") {
+      setLtdList(ltds.filter((ltd: Ltd) => ltd.joke.length % 2 === 1));
+    } else setLtdList(ltds);
+  }, [ltdType, ltds]);
 
   if (error) return <h1>An error has occurred.</h1>;
-  if (!ltds) return <LoadingNewMatchList />;
+  if (!ltdList) return <LoadingNewMatchList />;
 
   return (
     <Stack mb="4">
-      <Text
-        fontSize="2xl"
-        fontFamily={"heading"}
-        color={`${MAIN_COLOR}.500`}
-        fontWeight="bold"
-      >
-        新しいマッチ
-      </Text>
-      <Flex wrap="nowrap" overflowX="auto">
-        {ltds.map((ltd: Ltd) => (
-          <NavLink to={`/message/${ltd.id}`} key={ltd.id}>
-            <VStack text="center" display="inline-block" mr="5" minW="100px">
-              <Image
-                w="100px"
-                h="150px"
-                borderRadius="lg"
-                src={`https://icanhazdadjoke.com/j/${ltd.id}.png`}
-                fit="cover"
-              />
-              <Text align="center">{ltd.joke.slice(0, 10)}</Text>
-            </VStack>
-          </NavLink>
-        ))}
+      <Flex align="center" justify="space-between">
+        <Text
+          fontSize="2xl"
+          fontFamily={"heading"}
+          color={`${MAIN_COLOR}.500`}
+          fontWeight="bold"
+          mr="4"
+        >
+          新しいマッチ
+        </Text>
+        <Menu>
+          <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+            絞り込み
+          </MenuButton>
+          <MenuList>
+            <MenuItem
+              icon={
+                <CheckIcon
+                  color={ltdType === "all" ? "gray.600" : "gray.100"}
+                />
+              }
+              onClick={() => setLtdType("all")}
+            >
+              全ての企業
+            </MenuItem>
+            <MenuItem
+              icon={
+                <CheckIcon
+                  color={ltdType === "like" ? "gray.600" : "gray.100"}
+                />
+              }
+              onClick={() => setLtdType("like")}
+            >
+              ライクした企業
+            </MenuItem>
+            <MenuItem
+              icon={
+                <CheckIcon
+                  color={ltdType === "slike" ? "gray.600" : "gray.100"}
+                />
+              }
+              onClick={() => setLtdType("slike")}
+            >
+              スーパーライクした企業
+            </MenuItem>
+          </MenuList>
+        </Menu>
       </Flex>
+      <MatchListWrap ltds={ltdList} />
     </Stack>
   );
 };
