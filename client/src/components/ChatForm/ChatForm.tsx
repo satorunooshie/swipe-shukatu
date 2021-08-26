8c94eef6ec2bdd43007998f87e11179b59f8e6e1
hiromu <hiromhiro617@gmail.com>
2021-08-26 23:06:38 +0900

    remind message

--- client/src/components/ChatForm/ChatForm.tsx
+++ client/src/components/ChatForm/ChatForm.tsx
@@ -1,4 +1,4 @@
-import { VFC } from "react";
+import { VFC, useState } from "react";
 import {
   Flex,
   IconButton,
@@ -6,17 +6,34 @@ import {
   Button,
   FormControl,
   FormErrorMessage,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  Switch,
+  PopoverArrow,
+  PopoverCloseButton,
+  useDisclosure,
+  FormLabel,
 } from "@chakra-ui/react";
 import { MAIN_COLOR } from "../../constants/MainColor";
-import { AttachmentIcon } from "@chakra-ui/icons";
+import { AttachmentIcon, BellIcon } from "@chakra-ui/icons";
 import { FaTelegramPlane } from "react-icons/fa";
 import { useForm } from "react-hook-form";
+import Datetime from "react-datetime";
+import "react-datetime/css/react-datetime.css";
+import moment from "moment";
+import "moment/locale/ja";
 
 type FormData = {
   message: string;
 };
 
 const ChatForm: VFC = () => {
+  const [isReminded, setIsReminded] = useState(false);
+  const { onOpen, onClose, isOpen } = useDisclosure();
+  const [datetime, setDatetime] = useState<string>(moment().format());
   const {
     handleSubmit,
     register,
@@ -28,7 +45,26 @@ const ChatForm: VFC = () => {
     // TODO: API call
     return new Promise((resolve) => {
       setTimeout(() => {
-        alert(JSON.stringify(data));
+        if (isReminded) {
+          alert(
+            JSON.stringify({
+              type: 1,
+              content: data.message,
+              datetime: datetime,
+            })
+          );
+        } else {
+          alert(
+            JSON.stringify({
+              type: 2,
+              content: data.message,
+            })
+          );
+        }
+        // もろもろリセットする
+        onClose();
+        setIsReminded(false);
+        setDatetime(moment().format());
         setValue("message", "");
         resolve(1);
       }, 2000);
@@ -56,10 +92,61 @@ const ChatForm: VFC = () => {
           borderTop="1px solid"
           borderColor="gray.200"
         >
+          <Popover
+            isOpen={isOpen}
+            onOpen={onOpen}
+            onClose={onClose}
+            placement="top"
+            closeOnBlur={false}
+          >
+            <PopoverTrigger>
+              <IconButton
+                variant="ghost"
+                aria-label="remind"
+                icon={<BellIcon w={6} h={6} />}
+                color={isReminded ? `${MAIN_COLOR}.400` : "gray.500"}
+              />
+            </PopoverTrigger>
+            <PopoverContent p={5}>
+              <PopoverArrow />
+              <PopoverCloseButton />
+              <PopoverHeader fontWeight="bold">
+                メッセージのリマインド
+              </PopoverHeader>
+              <PopoverBody textAlign="center" py="4">
+                <Datetime
+                  initialValue={new Date()}
+                  locale="ja"
+                  onChange={(value) => {
+                    if (typeof value === "string") console.log(value);
+                    else setDatetime(value.format());
+                  }}
+                />
+                <FormControl
+                  display="flex"
+                  align="center"
+                  justifyContent="center"
+                  w="full"
+                  mt="3"
+                >
+                  <FormLabel htmlFor="remind-switch" mb="0">
+                    にリマインドする
+                  </FormLabel>
+                  <Switch
+                    id="remind-switch"
+                    size="md"
+                    isChecked={isReminded}
+                    onChange={() => setIsReminded(!isReminded)}
+                  />
+                </FormControl>
+              </PopoverBody>
+            </PopoverContent>
+          </Popover>
           <IconButton
             variant="ghost"
             aria-label="attach image"
-            icon={<AttachmentIcon />}
+            color="gray.500"
+            icon={<AttachmentIcon w={6} h={6} />}
           />
           <Input
             variant="filled"
