c40280bfc725d37f52225d3f27777b7898aa5d9c
joryulife <joryulife@gmail.com>
2021-08-27 10:08:43 +0900

    model削除

--- pkg/domain/model/matchlist_model.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Matchlist struct {
-	LtdID        int32  `json:"ltd_id"`
-	RecruitID    int32  `json:"recruit_id"`
-	Name         string `json:"name"`
-	Image        string `json:"image"`
-	Reactiontype int32  `json:"reactiontype"`
-}
