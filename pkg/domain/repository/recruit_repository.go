a258dad379acb530213bb4582fd657ac5a2917dc
joryulife <joryulife@gmail.com>
2021-08-28 01:20:36 +0900

    v1

--- pkg/domain/repository/recruit_repository.go
+++ pkg/domain/repository/recruit_repository.go
@@ -9,8 +9,33 @@ import (
 
 type RecruitRepository interface {
 	Select(ctx context.Context) ([]*recruitM.Recruit, error)
+	SelectRecruits(ctx context.Context, Param *Parameters) (*Recruits, error)
 	SelectRecruitForMessage(ctx context.Context, rID int32) (*recruitM.Recruit, error)
 	Insert(ctx context.Context, entity *recruitM.Recruit) error
 	Update(ctx context.Context, entity *recruitM.Recruit) error
 	Delete(ctx context.Context, entity *recruitM.Recruit) error
+}
+
+type Recruits struct {
+	LtdID                 int32  `json:"ltd_id"`
+	RecruitID             int32  `json:"recruit_id"`
+	Name                  string `json:"name"`
+	Profile               string `json:"profile"`
+	EmployeeNumber        int32  `json:"employee_number"`
+	AverageAge            int32  `json:"average_age"`
+	JobType               string `json:"job_type"`
+	EducationalBackground string `json:"educational_background"`
+	AverageSalary         int32  `json:"average_salary"`
+	StartingSalary        int32  `json:"starting_salary"`
+	Image                 string `json:"image"`
+}
+
+type Parameters struct {
+	Location         int
+	JobType          int
+	EducationHistory int
+	Benefits         int
+	MinSalary        int
+	MaxSalary        int
+	StartingSalary   int
 }
--- pkg/interfaces/handler/recruit_handler.go
+++ pkg/interfaces/handler/recruit_handler.go
@@ -2,8 +2,14 @@
 package handler
 
 import (
+	"context"
+	"encoding/json"
+	"log"
 	"net/http"
+	"strconv"
+	"time"
 
+	recruitR "github.com/satorunooshie/swipe-shukatu/pkg/domain/repository"
 	recruitU "github.com/satorunooshie/swipe-shukatu/pkg/usecase"
 )
 
@@ -28,7 +34,46 @@ func NewRecruitHandler(recruitU recruitU.RecruitUseCase) RecruitHandler {
 // HandleSelect
 func (recruitH *recruitHandler) HandleSelect() http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
-		panic("do something")
+		ctx := request.Context()
+		ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+		defer cancel()
+		parameters := new(recruitR.Parameters)
+		location, _ := strconv.Atoi(request.FormValue("location"))
+		parameters.Location = location
+		job_type, _ := strconv.Atoi(request.FormValue("job_type"))
+		parameters.JobType = job_type
+		education_history, _ := strconv.Atoi(request.FormValue("education_history"))
+		parameters.EducationHistory = education_history
+		benefits
+		ms, err := messageH.messageUseCase.Select(ctx, int32(rID))
+		if err != nil {
+			log.Printf("[ERROR] failed to fetch messages by recruit ID: %v", err.Error())
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		mrs := make([]*MessageResponse, len(ms))
+		for i, m := range ms {
+			var mr MessageResponse
+			mr.LtdID = m.LtdID
+			mr.RecruitID = m.RecruitID
+			mr.Name = m.Name
+			mr.JobType = m.JobType
+			mr.Content = m.Content
+			mr.Image = m.Image
+			mr.CreatedAt = m.CreatedAt
+			mrs[i] = &mr
+		}
+		var respms MessageResponses
+		respms.Messages = mrs
+		jrespms, err := json.Marshal(respms)
+		if err != nil {
+			log.Printf("[ERROR] failed to marshal messages: %v", err.Error())
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		writer.WriteHeader(http.StatusOK)
+		writer.Header().Set("Content-Type", "application/json")
+		writer.Write(jrespms)
 	}
 }
 
